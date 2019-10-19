23216dd9f2beb87ee7e8362431a0ece04b2830db
Ryan Schwartz <schwar95@purdue.edu>
2019-10-18 22:46:26 -0400

    Remove www and node_modules folder

--- Front-End/fab/src/app/tab1/tab1.page.ts
+++ Front-End/fab/src/app/tab1/tab1.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page
+{
 
-  constructor() {}
+  constructor(private localNotifications: LocalNotifications) { }
+
+  single_notification() {
+    // Schedule a single notification
+    this.localNotifications.schedule({
+      id: 1,
+      text: 'Single ILocalNotification',
+      sound: '',
+      data: { secret: 'key_data' }
+    });
+  }
 
 }
