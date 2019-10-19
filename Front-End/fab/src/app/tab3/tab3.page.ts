6c1a399901d541d82c9db196afd89deddfdaeda0
Ryan Schwartz <schwar95@purdue.edu>
2019-10-19 10:51:30 -0400

    Update front end to talk to server (localhost:3000)

--- Front-End/fab/src/app/tab3/tab3.page.ts
+++ Front-End/fab/src/app/tab3/tab3.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
 
-  constructor() {}
+  NAME: string;
+  ADDRESS: string;
+  MOBILE: string;
+  EMAIL: string;
+
+  constructor(private http: HttpClient) {
+    try {
+      this.http.get('http://localhost:3000/api/v1/124').subscribe((response) => {
+        this.NAME = response['ClientName'];
+        this.ADDRESS = response['Address'];
+        this.MOBILE = response['PhoneNumber'];
+        this.EMAIL = response['Email'];
+      });
+    }
+    catch(e)
+    {
+      this.NAME = "Server not set up properly."
+    }
+  }
+
+  ngOnInit(){
+
+  }
 
 }
