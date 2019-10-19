fb7a1150404b32e41b339f298bc6ae85966911ad
Dylan Rogers <dr233615@ohio.edu>
2019-10-19 08:04:40 -0400

    Added basic page link. Created credit debt page folder.

---
+++ Front-End/fab/src/app/credit-debt/credit-debt.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-credit-debt',
+  templateUrl: './credit-debt.page.html',
+  styleUrls: ['./credit-debt.page.scss'],
+})
+export class CreditDebtPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
