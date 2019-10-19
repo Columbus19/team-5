1e2f1159ab4d671e10100a7e3e932a1c38284cd7
Ryan Schwartz <schwar95@purdue.edu>
2019-10-19 10:52:09 -0400

    Update Back-End values to emulate the inputs from the front end

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,16 +1,20 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors());
+
 app.get('/api/v1/:clientId', (req, res) => {
     let mock_data = {
-        'ClientName': 12,
-        'Address': '1110 Polaris Parkway',
+        'ClientName': 'JP Morgan Chase',
+        'Email': 'jpmorgan@chase.com',
+        'Address': '1000 Polaris Pkwy, Columbus, OH 43240',
         'PhoneNumber': '+16141234567',
         'TotalDebt': 525.74,
         'MonthlyPayment': 15
     }
-    res.send(mock_data);
+    res.json(mock_data);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
