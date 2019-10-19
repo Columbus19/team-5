5ac368a0e3b6bdb9a8791a364324f5025a7f4a51
Dylan Rogers <dr233615@ohio.edu>
2019-10-19 05:47:08 -0400

    Fixed broken stuff

--- Front-End/fab/src/app/app-routing.module.ts
+++ Front-End/fab/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) }, 
   { path: 'developers', loadChildren: './pages/developers/developers.module#DevelopersPageModule' },
   { path: 'developer', loadChildren: './pages/developer/developer.module#DeveloperPageModule' },
+  { path: 'income', loadChildren: './income/income.module#IncomePageModule' },
   { path: 'income', loadChildren: './income/income.module#IncomePageModule' }
 ];
 @NgModule({
